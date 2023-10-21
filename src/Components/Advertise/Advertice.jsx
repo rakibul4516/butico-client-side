ab1b3dd6218289a05a5fd9a8d27318afef5aa273
Rakibul Islam <rakibul4516>
2023-10-21 14:31:57 +0600

    This commit for Advartise image size changes

--- src/Components/Advertise/Advertice.jsx
+++ src/Components/Advertise/Advertice.jsx
@@ -2,23 +2,23 @@
 const Advertice = () => {
     return (
         <div>
-            <div className="carousel w-full h-[100vh] ">
+            <div className="carousel w-full md:h-[100vh] h-96 ">
                 <div id="slide1" className="carousel-item relative w-full">
-                    <img src="https://i.ibb.co/CBSry71/banner3.jpg" className="w-full h-[100vh] object-cover" />
+                    <img src="https://i.ibb.co/CBSry71/banner3.jpg" className="w-full md:h-[100vh] h-96 object-cover" />
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                         <a href="#slide4" className="btn btn-circle ">❮</a>
                         <a href="#slide2" className="btn btn-circle">❯</a>
                     </div>
                 </div>
                 <div id="slide2" className="carousel-item relative w-full">
-                    <img src="https://i.ibb.co/ZJJh91q/banner1.jpg" className="w-full h-[100vh] object-cover" />
+                    <img src="https://i.ibb.co/ZJJh91q/banner1.jpg" className="w-full md:h-[100vh] h-96 object-cover" />
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                         <a href="#slide1" className="btn btn-circle">❮</a>
                         <a href="#slide3" className="btn btn-circle">❯</a>
                     </div>
                 </div>
                 <div id="slide3" className="carousel-item relative w-full">
-                    <img src="https://i.ibb.co/Jn8HCNR/banner3.jpg" className="w-full h-[100vh] object-cover" />
+                    <img src="https://i.ibb.co/Jn8HCNR/banner3.jpg" className="w-full md:h-[100vh] h-96 object-cover" />
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                         <a href="#slide3" className="btn btn-circle">❮</a>
                         <a href="#slide1" className="btn btn-circle">❯</a>
